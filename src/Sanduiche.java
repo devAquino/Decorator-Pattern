aaa12c42fafd6e9753e8cf17d7f89b35509604cf
devAquino <aquinoreginaldo@gmai.com>
2020-08-25 20:44:55 -0300

    removento atributos não utilizados

--- src/Sanduiche.java
+++ src/Sanduiche.java
@@ -1,12 +1,6 @@
 
 public abstract class Sanduiche {
 	
-	private String nome;
-	
-	public String getNome() {
-		return nome;
-	}
-	
 	abstract double mostrarPreco();
 	abstract String mostrarIngredientes();
 	
